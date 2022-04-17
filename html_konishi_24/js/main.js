85931e483b6c7cfadde2f69094bd67bef164fff0
KONIMAO <mao25987801@icloud.com>
2022-04-17 23:03:56 +0900

    first commit

---
+++ html_konishi_24/js/main.js
@@ -0,0 +1,12 @@
+// jsを記述する際はここに記載していく
+
+function cheeseflow(element){
+    cheeseflowKesi();
+    cheeseflowTukuru(element);
+};
+function cheeseflowKesi(){
+    for(var i=1; i<3; i++){
+        var syokiElement =document.getElementById('chedder-inner'); 
+        syokiElement.style.display = 'none';
+    }
+};
